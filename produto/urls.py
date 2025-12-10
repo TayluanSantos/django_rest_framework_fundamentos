89cd1003388f6f57377866408649aedd3d42a2a8
TayluanSantos <tayluanjesus@gmail.com>
2025-12-10 14:08:47 -0300

    refactor: refatorando o arquivo de rotas da aplicação

--- produto/urls.py
+++ produto/urls.py
@@ -1,23 +1,13 @@
 from django.urls import path,include
-# from .views import ProdutoList,ProdutoDetail
 from rest_framework.routers import DefaultRouter
 from . import views
 
-# Ao trabalhar com ViewSets, posso utilizar o router para criar as rotas de forma automática
-router = DefaultRouter() # Instanciando o DefaultRouter
+router = DefaultRouter() 
 router.register('produtos', views.ProdutoViewSet, basename='produtos') # Com base nos métodos definidos no ProdutoViewset (implicitamente) será criada as rotas.
-
 
 # Define a lista de rotas
 urlpatterns = [
     path('', include(router.urls)) # Incluindo as rotas no urlpatterns
 ]
-
-# Utilizando a abordagem de criar manualmente as rotas para a GenericView e APIView
-# urlpatterns = [
-#     path('produtos/',ProdutoList.as_view(),name='produtos'),
-#     path('produtos/<int:pk>/',ProdutoDetail.as_view(),name='produto')
-
-# ]
 
 
