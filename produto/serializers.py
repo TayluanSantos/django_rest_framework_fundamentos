9b8c18956286a31f8eb0ab9ddb7d80ef0fc7e881
TayluanSantos <tayluanjesus@gmail.com>
2025-12-10 14:06:27 -0300

    refactor: refatorando a classe serializer

--- produto/serializers.py
+++ produto/serializers.py
@@ -1,22 +1,20 @@
 from rest_framework import serializers
 from .models import Produto
 
-# Criando os serializers de forma mais abstraída, utilizando os ModelSerializer
 class ProdutoSerializer(serializers.ModelSerializer):
     
     class Meta:
       model = Produto
-      fields = '__all__' # O valor '__all__' indica que todos os campos serão serializados e desserilizados 
+      fields = ['id','nome','descricao','preco','estoque']
 
+    def validate_estoque(self,value):
+       
+       if value <= 0 :
+          raise serializers.ValidationError('O valor de estoque deve ser maior que 0')
+       
+       return value
+    
+    
 
-# Testando a abordagem de criar os serializers de forma manual
-# class ProductSerializer(serializers.Serializer):
-
-#   name = serializers.CharField(max_length=200,required=True,allow_blank=False,)
-#   description = serializers.CharField(required=False,allow_blank=True,allow_null=True)
-#   price = serializers.DecimalField(max_digits=9,decimal_places=2,required=True)
-#   inventory = serializers.IntegerField(required=True)
-
-
-
+    
 
