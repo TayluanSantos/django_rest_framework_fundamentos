fd33e16940f52da46e08622f19cff3d93591f147
TayluanSantos <tayluanjesus@gmail.com>
2025-12-12 12:47:39 -0300

    feat: criando relacionamento entre Produto e Categoria

--- produto/models.py
+++ produto/models.py
@@ -2,22 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Produto (models.Model):
-
-    nome = models.CharField(max_length=255) 
-    descricao = models.TextField(max_length=1000,blank=True,null=True)
-    preco = models.DecimalField(max_digits=9,decimal_places=2)
-    quantidade_estoque = models.IntegerField()
-    criado_em = models.DateField(auto_now_add=True)
-    atualizado_em = models.DateField(auto_now=True)
-
-    class Meta:
-        verbose_name = 'Produto'
-        verbose_name_plural = 'Produtos'
-    
-    def __str__(self):
-        return self.nome
-
 class Categoria (models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.CharField(max_length=500)
@@ -31,5 +15,22 @@ class Categoria (models.Model):
 
     def __str__(self):
         return self.nome
+
+class Produto (models.Model):
+
+    nome = models.CharField(max_length=255) 
+    descricao = models.TextField(max_length=1000,blank=True,null=True)
+    preco = models.DecimalField(max_digits=9,decimal_places=2)
+    quantidade_estoque = models.IntegerField()
+    criado_em = models.DateField(auto_now_add=True)
+    atualizado_em = models.DateField(auto_now=True)
+    categoria = models.ManyToManyField(Categoria)
+
+    class Meta:
+        verbose_name = 'Produto'
+        verbose_name_plural = 'Produtos'
     
+    def __str__(self):
+        return self.nome
+
     
