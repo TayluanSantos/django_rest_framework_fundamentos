33de843d6b675d01cd15e292e8de5aada4e08143
TayluanSantos <tayluanjesus@gmail.com>
2025-12-12 12:41:50 -0300

    feat: criando o model Categoria

--- produto/models.py
+++ produto/models.py
@@ -17,3 +17,19 @@ class Produto (models.Model):
     
     def __str__(self):
         return self.nome
+
+class Categoria (models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=500)
+    criado_em = models.DateField(auto_now_add=True)
+    atualizado_em = models.DateField(auto_now=True)
+
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+
+    def __str__(self):
+        return self.nome
+    
+    
